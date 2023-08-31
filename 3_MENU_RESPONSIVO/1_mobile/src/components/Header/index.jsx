a6491793494f5dece9713c1b907ede3c01d4dfb4
Marcio Garcia <marciomarcal.dev@gmail.com>
2023-08-31 18:10:22 -0300

    menu funcionando

--- 3_MENU_RESPONSIVO/1_mobile/src/components/Header/index.jsx
+++ 3_MENU_RESPONSIVO/1_mobile/src/components/Header/index.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react"
 import "./index.css"
 
 function Header() {
+    const [abrirFechar, setAbrirFechar] = useState(false)
+
+    function handleAbrirFecharMenu() {
+        if(abrirFechar) {
+            setAbrirFechar(false)
+            return
+        }
+
+        setAbrirFechar(true)
+    }
+
   return (
     <header>
         <div className="menu-mobile">
             <div className="botao-menu">
-                <button>Abrir Menu</button>
+                <button onClick={handleAbrirFecharMenu}>{abrirFechar == true ? "Fechar Menu" : "Abrir Menu"}</button>
             </div>
 
-            <div className="menu">
+            <div className={`menu ${abrirFechar == true ? "" : "close"}`}>
                 <nav>
                     <a href="">Configurações</a>
                     <a href="">Login</a>
